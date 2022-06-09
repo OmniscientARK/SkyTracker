e9969a028557973eec86e8211538598bc72a1f38
OmniscientARK <lorenzopiuga@gmail.com>
2022-06-09 17:21:10 -0300

    Fix path

--- dataManager.js
+++ dataManager.js
@@ -5,8 +5,7 @@ const keyManager = require("./keyManager")
 
 class DataManager{
     constructor(path){
-        this.dir = `/${path}`
-        console.log(this.dir)
+        this.dir = `${__dirname}/${path}`
         this.uuidSheet = `${this.dir}/uuids.json`
         this.nameSheet = `${this.dir}/names.json`
         if(!fs.existsSync(`${this.dir}`)) fs.mkdirSync(`${this.dir}`, {recursive: true})
